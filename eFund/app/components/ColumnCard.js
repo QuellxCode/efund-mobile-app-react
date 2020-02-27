161448072957f227f7f00f69cbc51d95f37ef7e3
BitesDigital <BitesDigital@users.noreply.github.com>
2020-02-27 20:16:33 +0500

    First Commit

---
+++ eFund/app/components/ColumnCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, TouchableOpacity, Image, Text } from 'react-native';
+import MainFlowStyles from '../Styles/MainFlowStyles';
+import { withNavigation } from 'react-navigation';
+
+const ColumnCard = ({ navigation }) => {
+    return (
+        <>
+            <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+                <TouchableOpacity
+                    style={[MainFlowStyles.cardStyle, { flex: 1, padding: 30, paddingVertical: 50, alignItems: 'center', justifyContent: 'center', marginRight: 20 }]}
+                    onPress={() => navigation.navigate('GeneratePayOrder')}
+                >
+                    <Image
+                        source={require('../images/GenerateOrderBig.png')}
+                        style={{ marginBottom: 20 }}
+                    />
+                    <Text style={MainFlowStyles.cardTextStyle}>{'Generate\nOrder'}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={[MainFlowStyles.cardStyle, { flex: 1,padding: 30, paddingVertical: 50, alignItems: 'center', justifyContent: 'center' }]}
+                    onPress={() => navigation.navigate('Reports')}
+                >
+                    <Image
+                        source={require('../images/ReportsBig.png')}
+                        style={{ marginBottom: 20 }}
+                    />
+                    <Text style={MainFlowStyles.cardTextStyle}>{'Reports'}</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+                <TouchableOpacity
+                    style={[MainFlowStyles.cardStyle, { flex: 1, padding: 30, paddingVertical: 50, alignItems: 'center', justifyContent: 'center', marginRight: 20 }]}
+                    onPress={() => navigation.navigate('Settings')}
+                >
+                    <Image
+                        source={require('../images/SettingsBig.png')}
+                        style={{ marginBottom: 20 }}
+                    />
+                    <Text style={MainFlowStyles.cardTextStyle}>{'Settings'}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{ flex: 1,padding: 30, paddingVertical: 50, alignItems: 'center', justifyContent: 'center' }}
+                />
+                    
+            </View>
+        </>
+    );
+};
+
+export default withNavigation(ColumnCard);
---
+++ eFund/app/screens/MainFlow/DashboardScreen.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView, StyleSheet, AsyncStorage } from 'react-native';
+import Header from '../../components/Header';
+import Timeline from '../../components/Timeline';
+import TwoColumnCard from '../../components/TwoColumnCard';
+import ColumnCard from '../../components/ColumnCard';
+import MainFlowStyles from '../../Styles/MainFlowStyles';
+
+class DashboardScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {id: 1, name: 'Purchaser', select: true},
+                {id: 2, name: 'Manager', select: false},
+                {id: 3, name: 'Director', select: false},
+                {id: 4, name: 'Accountant', select: false}
+            ],
+            User: []
+        };
+    }
+
+    componentDidMount() {
+        this._retrieveData();
+      }
+   
+     _retrieveData = async () => {
+       try {
+         const value = await AsyncStorage.getItem('User');
+         const val = JSON.parse(value)
+         if (val !== null) {
+           this.setState({
+             User: val,
+            })
+         }
+       } catch (error) {
+         console.log('error getting data')
+       }
+     };
+
+    render() {
+        console.log(this.state.User.roles)
+        if(this.state.User.roles == "purchaser" || this.state.User.roles == "Supervisor"){
+        return (
+            <View>
+                <Header />
+                <ScrollView
+                    style={{ paddingBottom: 30, marginBottom: 40 }}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <View style={{ marginHorizontal: 20 }}>
+                        <Text style={styles.welcomeTextStyle}>Welcome!</Text>
+                        
+                        {/* TIMELINE */}
+                        <View style={[MainFlowStyles.cardStyle, { paddingBottom: 10, marginBottom: 20 }]}>
+                            <View style={styles.currentStatusContainer}>
+                                <Text style={styles.currentStatusTextStyle}>Current Status</Text>
+                            </View>
+                            <View style={{ padding: 10, alignItems: 'center' }}>
+                                <Timeline data={this.state.data} />
+                            </View>
+                        </View>
+
+                        {/* Cards */}
+                        <TwoColumnCard />
+                    </View>
+                </ScrollView>
+            </View>
+        );
+        }
+        if(this.state.User.roles == "Director"){
+            return (
+                <View>
+                    <Header />
+                    <ScrollView
+                        style={{ paddingBottom: 30, marginBottom: 40 }}
+                        showsVerticalScrollIndicator={false}
+                    >
+                        <View style={{ marginHorizontal: 20 }}>
+                            <Text style={styles.welcomeTextStyle}>Welcome!</Text>
+    
+                            {/* Cards */}
+                            <ColumnCard />
+                        </View>
+                    </ScrollView>
+                </View>
+            );
+        }
+        return(
+            <View>
+                    <Header />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    welcomeTextStyle: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        marginVertical: 10
+    },
+    currentStatusContainer: {
+        borderBottomColor: '#FF3301',
+        borderBottomWidth: 1,
+        padding: 10
+    },
+    currentStatusTextStyle: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginLeft: 10
+    }
+});
+
+export default DashboardScreen;
